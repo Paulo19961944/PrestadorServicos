6cd7702e531183afba7d2d1f02c8d67074061969
Paulo19961944 <paulo24041996@gmail.com>
2025-09-22 00:58:41 -0300

    [feature] Delegando Responsablidades

--- src/main.js
+++ src/main.js
@@ -1,23 +1,4 @@
-const pageData = [
-  {
-    imageSrc: 'public/images/Prestador-Serviço-1.jpg',
-    imageAlt: 'Prestador de Serviço',
-    title: 'Encontre seu Prestador de Serviço',
-    paragraph: 'Encontre o prestador de serviços ideal sem sair de casa, com praticidade, rapidez e total conforto.'
-  },
-  {
-    imageSrc: 'public/images/Praticidade-1.jpg',
-    imageAlt: 'Prestador de Serviço',
-    title: 'Praticidade',
-    paragraph: 'Contratar um prestador de serviço nunca foi tão fácil: praticidade, agilidade e tudo ao seu alcance em poucos cliques.'
-  },
-  {
-    imageSrc: 'public/images/Otimizacao-Tempo-1.jpg',
-    imageAlt: 'Prestador de Serviço',
-    title: 'Otimização de Tempo',
-    paragraph: 'Ganhe tempo ao contratar serviços de forma rápida e eficiente, sem complicações.'
-  }
-];
+import { pageData } from "./page/pageData.js";
 
 export default function renderSections() {
   const main = document.querySelector('main');
